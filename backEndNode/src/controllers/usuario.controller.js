b4eb40c8936b905531c2bb85f16d6e3735602105
cesarchs <cesarchamalesican@gmail.com>
2022-09-14 17:45:13 -0600

    Se agrego peticion para pantalla principal de los archivos
    peticion de archivos por id

--- backEndNode/src/controllers/usuario.controller.js
+++ backEndNode/src/controllers/usuario.controller.js
@@ -52,6 +52,40 @@ appUsuario.post('/register',(request, response)=>{
 })
 
 // ARCHIVOS DE USUARIO
+appUsuario.get('/userFiles/:idUser',(request, response)=>{
+    var idUser = request.params.idUser;
+    var miQuery = "SELECT idArchivo, file_name, private, URL, date_format(FechaCreacion, '%d/%m/%Y') as FechaCreada , date_format(FechaModificacion, '%d/%m/%Y') as FechaModificacion " +
+    "FROM ARCHIVO WHERE propietario = '" +idUser+"'"+" ORDER BY private ASC;"
+    ;
+    console.log(miQuery);
+    conn.query(miQuery, function(err, result){
+        if(err || result[0] == undefined){
+            console.log(err);
+            response.status(502).send('Status: false');
+        }else{
+            console.log(result);
+            response.status(200).send(result);
+        }
+    }); 
+})
+/* // CONSULTA
+SELECT idArchivo, file_name, private, URL, date_format(FechaCreacion, '%d/%m/%Y') as FechaCreada , date_format(FechaModificacion, '%d/%m/%Y') as FechaModificacion
+FROM ARCHIVO WHERE propietario = '6' ORDER BY private ASC;
+*/
 
+/*
+appHotel.get('/getResenaHotel/:id',(request, response)=>{
+    var idservicio = request.params.id;
+    var miQuery = "prcedure getResena ("+idservicio+");";
+    conexion.query(miQuery, function(err, result){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(result);
+            response.send(result);
+        }
+    }); 
+})
+*/
 
 export default appUsuario
